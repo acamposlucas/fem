790e73a97c1dc096d4e5758198b6ad946b51e697
lucasalmeida <lac.lucas96@gmail.com>
2023-06-15 20:10:36 -0300

    Create Header component

---
+++ planet-facts-site/src/components/Header.tsx
@@ -0,0 +1,21 @@
+function Header() {
+	return (
+		<header>
+			<div className="main__header container">
+				<h1>The Planets</h1>
+				<button type="button">
+					<span className="visually-hidden">Toggle navigation</span>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="17">
+						<g fill="#FFF" fillRule="evenodd">
+							<path d="M0 0h24v3H0zM0 7h24v3H0zM0 14h24v3H0z" />
+						</g>
+					</svg>
+				</button>
+			</div>
+		</header>
+	);
+}
+export default Header;
