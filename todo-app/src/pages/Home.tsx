132ef32bdee36f450ff5a27556d6ea61f6be8b11
lucasalmeida <lac.lucas96@gmail.com>
2023-08-13 15:42:40 -0300

    todo-app: ajusta input checkbox

--- todo-app/src/pages/Home.tsx
+++ todo-app/src/pages/Home.tsx
@@ -6,6 +6,14 @@ import { useState } from "react";
 
 type selectedMenu = "all" | "active" | "completed";
 
+const TODOS = [
+  { id: 1, isDone: false, description: "Javascript course" },
+  { id: 2, isDone: false, description: "Javascript course" },
+  { id: 3, isDone: false, description: "Javascript course" },
+  { id: 4, isDone: false, description: "Javascript course" },
+  { id: 5, isDone: false, description: "Javascript course" },
+];
+
 function HomePage({
   isDarkMode,
   handleDarkMode,
@@ -14,9 +22,17 @@ function HomePage({
   handleDarkMode: () => void;
 }) {
   const [selectedMenu, setSelectedMenu] = useState<selectedMenu>("all");
+  const [todos, setTodos] = useState<any[]>(TODOS);
 
   const handleSelectedMenu = (menu: selectedMenu) => {
     setSelectedMenu(menu);
+  };
+
+  const handleToggleTodoIsDoneStatus = (id: number) => {
+    const LIST = [...todos];
+    const todo = LIST.find((todo) => todo.id === id);
+    todo.isDone = !todo.isDone;
+    setTodos(LIST);
   };
 
   return (
@@ -45,16 +61,40 @@ function HomePage({
         </div>
         <div className="mt-4 overflow-auto rounded-lg">
           <ul>
-            {[1, 2, 3, 4, 5].map((v) => (
+            {todos.map((todo: any) => (
               <li
-                key={v}
+                key={todo.id}
                 className="flex items-center justify-between border-b-[1px] border-zinc-700 bg-zinc-800 px-5 py-4 text-zinc-400 text-xs last:border-none"
               >
-                <div>
-                  <label htmlFor=""></label>
-                  <input type="checkbox" id="" className="me-3" />
-                  Complete online Javascript course
-                </div>
+                <span className="flex">
+                  <div className="relative me-3 inline-block h-5 w-5">
+                    <input
+                      type="checkbox"
+                      defaultChecked={todo.isDone}
+                      onChange={() => handleToggleTodoIsDoneStatus(todo.id)}
+                      id={String(todo.id)}
+                      className="peer absolute left-0 top-0 h-[1px] w-[1px] opacity-0"
+                    />
+                    <label
+                      htmlFor={String(todo.id)}
+                      className="inline-flex h-5 w-5 items-center justify-center rounded-full border-[1px] border-zinc-500 peer-checked:bg-gradient-to-tr peer-checked:from-[#57ddff] peer-checked:to-[#c058f3] [&>*:first-child]:hidden peer-checked:[&>*:first-child]:block"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="12"
+                        height="8"
+                      >
+                        <path
+                          fill="none"
+                          stroke="#FFF"
+                          strokeWidth="2"
+                          d="M1 4.304L3.696 7l6-6"
+                        />
+                      </svg>
+                    </label>
+                  </div>
+                  <span>Complete online Javascript course</span>
+                </span>
                 <button
                   type="button"
                   className="inline-flex items-center justify-center"
@@ -65,7 +105,9 @@ function HomePage({
               </li>
             ))}
             <li className="flex items-center justify-between border-b-[1px] border-zinc-700 bg-zinc-800 px-5 py-4 text-zinc-400 text-xs last:border-none">
-              <strong className="font-normal text-xs">5 items left</strong>
+              <strong className="font-normal text-xs">{`${todos.length} item${
+                todos.length > 1 ? "s" : ""
+              } left`}</strong>
               <button type="button" className="text-xs">
                 Clear completed
               </button>
