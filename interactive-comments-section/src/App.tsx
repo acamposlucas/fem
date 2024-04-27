488d40a7828aeed8cc7c90ecb1bf6662c5d0a604
lucasalmeida <lac.lucas96@gmail.com>
2024-04-27 18:59:38 -0300

    init react interactive comments section project

---
+++ interactive-comments-section/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="h-screen bg-zinc-300">
+        <h1 className="text-3xl">Hello World</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
